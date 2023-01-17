3966243e5ccd4d138f048fba0a8a681b33f63106
Stopalov12 <stopalov111@gmail.com>
2023-01-17 16:53:26 +0200

    Netflix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**',
+      },
+    ],
+  },
+};
